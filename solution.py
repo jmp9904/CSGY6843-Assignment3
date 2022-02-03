ee358b4f6c65f1a4259d716c970d95c318cd8c86
jmp9904 <95732066+jmp9904@users.noreply.github.com>
2022-02-03 14:26:10 -0600

    Add files via upload

--- solution.py
+++ solution.py
@@ -80,6 +80,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.send(quitCommand.encode())
     recv7 = clientSocket.recv(bufferSize).decode()
     #print (recv7)
+    clientSocket.close()
     # Fill in end
 
 
